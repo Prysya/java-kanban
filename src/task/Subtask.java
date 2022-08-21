481b840c592ca2985543eef507c0a6c2bd4632d9
Stas <bloodofabyss@yandex.ru>
2022-08-21 15:33:05 +0300

    feat: написал класс для подзадач

---
+++ src/task/Subtask.java
@@ -0,0 +1,32 @@
+package task;
+
+import enums.TaskStatus;
+
+/**
+ * Класс для подзадач класса {@link Epic}
+ */
+public class Subtask extends Task {
+    Integer parentEpicId;
+
+    public Subtask(String title, String description, int id, TaskStatus taskStatus) {
+        super(title, description, id, taskStatus);
+    }
+
+    /**
+     * @param parentEpicId идентификатор эпика родителя
+     */
+    public void setParentEpicId(Integer parentEpicId) {
+        this.parentEpicId = parentEpicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "title='" + super.getTitle() + '\'' +
+                ", description='" + super.getDescription() + '\'' +
+                ", id=" + super.getId() +
+                ", taskStatus=" + super.getTaskStatus() +
+                ", parentEpicId=" + (parentEpicId == null ? "null" : parentEpicId) +
+                '}';
+    }
+}
