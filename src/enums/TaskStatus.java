172622c112d3931ff4180fe5422c0a45379659e0
Stas <bloodofabyss@yandex.ru>
2022-08-21 15:32:41 +0300

    feat: написал enum для статусов задач

---
+++ src/enums/TaskStatus.java
@@ -0,0 +1,24 @@
+package enums;
+
+public enum TaskStatus {
+        NEW ("NEW"),
+        IN_PROGRESS ("IN_PROGRESS"),
+        DONE("DONE");
+
+        private final String status;
+
+        TaskStatus(String status) {
+                this.status = status;
+        }
+
+        public String getStatus() {
+                return status;
+        }
+
+        @Override
+        public String toString() {
+                return "TaskStatus{" +
+                        "status='" + status + '\'' +
+                        '}';
+        }
+}
