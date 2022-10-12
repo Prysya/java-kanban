59612e6d12dcbfcd5267b968f00556d1d0efc46f
Stas <bloodofabyss@yandex.ru>
2022-10-12 17:46:14 +0300

    chore: обновил методы парсинга в строку у всех задач

--- src/task/Subtask.java
+++ src/task/Subtask.java
@@ -1,17 +1,38 @@
 package task;
 
 import enums.TaskStatus;
+import enums.TaskType;
 
 /**
- * Класс для подзадач класса {@link Epic}
+ * Класс для подзадач класса {@link Epic}.
  */
 public class Subtask extends Task {
     /**
-     * Идентификатор эпика родителя
+     * Количество параметров в csv строке.
+     */
+    private static final int CSV_PARAMS_COUNT = 6;
+    /**
+     * Тип таска.
+     */
+    private static final TaskType TASK_TYPE = TaskType.SUBTASK;
+    /**
+     * Идентификатор эпика родителя.
      */
     private final Integer parentEpicId;
 
-    public Subtask(String title, String description, TaskStatus taskStatus, int parentEpicId) {
+    /**
+     * Конструктор класса {@link Subtask}.
+     * @param title заголовок подтаска
+     * @param description описание подтаска
+     * @param taskStatus статус подтаска
+     * @param parentEpicId уникальный идентификатор {@link Epic}
+     * */
+    public Subtask(
+            final String title,
+            final String description,
+            final TaskStatus taskStatus,
+            final int parentEpicId
+    ) {
         super(title, description, taskStatus);
         this.parentEpicId = parentEpicId;
     }
@@ -23,14 +44,45 @@ public class Subtask extends Task {
         return parentEpicId;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
-        return "Subtask{" +
-                "title='" + super.getTitle() + '\'' +
-                ", description='" + super.getDescription() + '\'' +
-                ", id=" + super.getId() +
-                ", taskStatus=" + super.getTaskStatus() +
-                ", parentEpicId=" + (parentEpicId == null ? "null" : parentEpicId) +
-                '}';
+        return getId() + ","
+                + TASK_TYPE.getStatus() + ","
+                + getTitle() + ","
+                + getTaskStatus().getStatus() + ","
+                + getDescription() + ","
+                + getParentEpicId();
+    }
+
+    /**
+     * Получение {@link Subtask} из строки.
+     * @param value строка в csv формате
+     * @return {@link Subtask}
+     */
+    public static Subtask fromString(final String value) {
+        String[] values = value.split(",");
+
+        final int idIndex = 0;
+        final int titleIndex = 2;
+        final int subtaskStatusIndex = 3;
+        final int descriptionIndex = 4;
+        final int parentEpicIdIndex = 5;
+
+        if (values.length < CSV_PARAMS_COUNT) {
+            return null;
+        }
+
+        Subtask subtask = new Subtask(
+                values[titleIndex],
+                values[descriptionIndex],
+                TaskStatus.fromString(values[subtaskStatusIndex]),
+                Integer.parseInt(values[parentEpicIdIndex])
+        );
+        subtask.setId(Integer.parseInt(values[idIndex]));
+
+        return subtask;
     }
 }
