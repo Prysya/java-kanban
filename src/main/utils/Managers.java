1fe276097094878c30efd9ce0413318ba2db6525
Stas <bloodofabyss@yandex.ru>
2022-11-11 01:38:21 +0300

    chore: поменял дефолтный класс в утилитарном классе менеджеров на HTTP менеджера

--- src/main/utils/Managers.java
+++ src/main/utils/Managers.java
@@ -1,15 +1,15 @@
 package main.utils;
 
 import main.manager.FileBackedTasksManager;
+import main.manager.HTTPTaskManager;
 import main.manager.InMemoryHistoryManager;
-import main.manager.InMemoryTaskManager;
 
 public final class Managers {
     private Managers() {
     }
 
-    public static InMemoryTaskManager getDefault() {
-        return new InMemoryTaskManager();
+    public static HTTPTaskManager getDefault() {
+        return new HTTPTaskManager("http://localhost:8078");
     }
 
     public static InMemoryHistoryManager getDefaultHistory() {
